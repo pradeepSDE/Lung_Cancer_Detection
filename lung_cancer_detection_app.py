3e0f3b53214324586c96002f77daff18609ddf5a
pradeep <pradeepbisen60@gmail.com>
2024-10-21 13:07:15 +0530

    fix model

--- lung_cancer_detection_app.py
+++ lung_cancer_detection_app.py
@@ -8,12 +8,15 @@ from PIL import Image
 import streamlit as st
 
 # Load your trained VGG16 model
-model = load_model('lung_cancer_vgg16_model.h5')
+model = load_model('lung_cancer_vgg16_model_1.h5')
 
 # Define class names
-classes = ['benign', 'squamous', 'adenocarcinoma']
+# classes = ['adenocarcinoma', 'squamous', 'benign']
+classes = ['adenocarcinoma','benign', 'squamous' ]
 
 # Function to preprocess the uploaded image
+
+
 def preprocess_image(img):
     img = img.resize((224, 224))  # Resize the image to VGG16 input size
     img_array = image.img_to_array(img)
@@ -22,30 +25,36 @@ def preprocess_image(img):
     return img_array
 
 # Function to classify the input image
+
+
 def classify_image(img):
     img_array = preprocess_image(img)
     predictions = model.predict(img_array)
+    print(predictions)
     predicted_class = np.argmax(predictions, axis=1)[0]
+    print(predicted_class)
     return classes[predicted_class], predictions[0]
+
 
 # Streamlit app
 st.title("Lung Cancer Detection Using VGG16")
 
 # Upload an image file
-uploaded_file = st.file_uploader("Please upload a histopathology image", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader(
+    "Please upload a histopathology image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     # Open and display the uploaded image
     img = Image.open(uploaded_file)
     st.image(img, caption='Uploaded Image', use_column_width=True)
-    
+
     # Classify image on button click
     if st.button('Classify Image'):
         result_class, probabilities = classify_image(img)
-        
+
         # Display results
         st.write(f"### Predicted Class: {result_class}")
         st.write(f"### Class Probabilities:")
-        st.write(f"Benign: {probabilities[0]:.4f}")
-        st.write(f"Squamous: {probabilities[1]:.4f}")
-        st.write(f"Adenocarcinoma: {probabilities[2]:.4f}")
+        st.write(f"Benign: {probabilities[1]:.4f}")
+        st.write(f"Squamous: {probabilities[2]:.4f}")
+        st.write(f"Adenocarcinoma: {probabilities[0]:.4f}")
